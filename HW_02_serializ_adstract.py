2783f234b3fcea9fecda654f255588489d8406c3
Nataliia Zharovska <natalkadtk@gmail.com>
2022-09-27 01:13:15 +0300

    st1

--- HW_02_serializ_adstract.py
+++ HW_02_serializ_adstract.py
@@ -14,22 +14,23 @@ class SerializationInterface(metaclass=ABCMeta):
 class JsonSerializ(SerializationInterface):
     def restore(self, file_name):
         with open(file_name, "rb+") as file:
-            return json.load(file)
+            json.load(file)
                 
         
     def save(self, file_name, data):
         with open(file_name, "wb") as file:
-            return json.dump(data, file) 
+            json.dump(data, file) 
             
 class BinSerializ(SerializationInterface):
     def restore(self, file_name):
         with open(file_name, "rb+") as file:
-            return pickle.load(file)
+            pickle.load(file)
                 
         
     def save(self, file_name, data):
         with open(file_name, "wb") as file:
-            return pickle.dump(data, file) 
+            pickle.dump(data, file,protocol=pickle.HIGHEST_PROTOCOL) 
+            print("savwd")
             
 some_data = {
     'flowers': '21', 
@@ -45,4 +46,5 @@ bin = BinSerializ()
 js.save(file_name='data3.json', data=some_data)
 bin.save(file_name='data3.bin', data=some_data)
 
-
+a= js.restore(file_name='data3.json')
+b= bin.restore(file_name='data3.bin')
